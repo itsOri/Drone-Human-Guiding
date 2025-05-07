005be13a446e14683e95e75d626e2f680ce6a116
NathanA15 <nathanadda@me.com>
2025-05-07 19:59:53 +0300

    added files: 'commands.txt' and basic_control.py
    Added commands.txt file with basic commands for start of project
    added dir drone_control with file basic_control
    to try and play with the drone
    added requirements.txt with library for tellu drone

---
+++ drone_control/basic_control.py
@@ -0,0 +1,13 @@
+# this is a file for running basic commands with the drone controls
+# Including fly, up, down, land...
+from djitellopy import Tello
+
+
+
+def main():
+	print("test")
+	
+
+
+if __name__ == '__main__':
+    main()
