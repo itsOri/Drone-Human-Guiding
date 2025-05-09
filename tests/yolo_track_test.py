0cdd0741da779f964dad2a3a616270bbe9ba2330
NathanA15 <nathanadda@me.com>
2025-05-09 14:22:07 +0300

    did multiple tests with different yolos for now the small version is really good need to test segmentation models, added gitignore file

--- tests/yolo_track_test.py
+++ tests/yolo_track_test.py
@@ -1,4 +1,4 @@
 from ultralytics import YOLO
 
-model = YOLO("yolo11n.pt")
-results = model.track("../tello_captureori2.avi", save=True, show=True)
+model = YOLO("yolo11s-seg.pt")
+results = model.track("tello_captureori2.avi", save=True, show=True)
