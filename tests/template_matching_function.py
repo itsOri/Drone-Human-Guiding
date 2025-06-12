2f88f12acc7bc4d4759693c01ad0db99ebbb144d
itsOri <orifri123@gmail.com>
2025-06-12 17:23:31 +0300

    added gaussian blur to the image

--- tests/template_matching_function.py
+++ tests/template_matching_function.py
@@ -12,13 +12,18 @@ def fix_size(image, template_width):
 
     return resized_image
 
+KERNEL_SIZE=15
 def pre_proccess(image, template_width):
+
     image_width = image.shape[1]
     if(image_width != template_width):
         image = fix_size(image, template_width)
     image = image.astype(np.float32) / 255.0
     image -= image.mean(axis=(0,1))
-    image = image / np.std(image)
+    if(np.std(image) != 0):
+        image = image / np.std(image)
+    image = cv2.GaussianBlur(image, (KERNEL_SIZE, KERNEL_SIZE), 0)  # kernel size (5,5), sigma=0
+
     return image
 
 def perform_correlation(template_obj, image):
@@ -73,13 +78,14 @@ def template_match(template_obj, image):
     template_obj = pre_proccess(template_obj, template_obj.shape[1])
     image = pre_proccess(image, template_obj.shape[1])
 
-    deg_arr = [0,2,-2,5,-5]
+    deg_arr = [0,1,-1,2,2,3,-3]
     min_vals_rot = np.zeros(len(deg_arr))
     for i in range(len(deg_arr)):
         degrees = deg_arr[i]
         rotated_template = rotate_template(template_obj, degrees)
         min_vals_rot[i] = perform_correlation(rotated_template, image)
     
+    #print(min_vals_rot)
     min_val = min_vals_rot.min()
     return min_val
 
@@ -93,11 +99,12 @@ for i in range(19):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     min_vals[i] =  template_match(template, image)
 
-# image = cv2.imread(f"photos/person_id_59_capture_11.jpg", cv2.COLOR_BGR2RGB)
-# image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-# min_vals[19] =  template_match(template, image)
+image = cv2.imread(f"photos/person_id_59_capture_11.jpg", cv2.COLOR_BGR2RGB)
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+min_vals[19] =  template_match(template, image)
 
-# min_val_index = np.argmin(min_vals)
-# np.set_printoptions(precision=2, suppress=True)
-# print(f"min score: {min_vals[min_val_index]}, photo: person_id_13_capture_{min_val_index+1}")
-# print(f"all scores: {np.round(min_vals, 2)}")
+min_val_index = np.argmin(min_vals)
+np.set_printoptions(precision=2, suppress=True)
+print(f"min score: {min_vals[min_val_index]}, photo: person_id_13_capture_{min_val_index+1}")
+print(f"all scores: {np.round(min_vals, 2)}")
+print(f"ratio bad/good is: {min_vals[19] / (min_vals[1:19].min())}")
